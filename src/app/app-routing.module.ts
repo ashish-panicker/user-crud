7b3e9be70c85068e634b19a78a660004c3312446
ashish-panicker <ashish.s.panicker@gmail.com>
2019-10-01 10:46:57 +0530

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeUserComponent } from './home-user/home-user.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', component:HomeUserComponent},
+  {path:'add', component:AddUserComponent},
+  {path:'list', component:ListUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+import { NavUserComponent } from './nav-user/nav-user.component';
+import { HomeUserComponent } from './home-user/home-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    ListUserComponent,
+    NavUserComponent,
+    HomeUserComponent
   ],
   imports: [
     BrowserModule,
